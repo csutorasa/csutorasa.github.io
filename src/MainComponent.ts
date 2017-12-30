632bb5c810c7f09a3ccac696434d06290cd1355a
Ármin Csutorás <csutorasa@gmail.com>
2017-12-30 17:39:18 +0100

    Github api test

--- src/MainComponent.ts
+++ src/MainComponent.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
 
 @Component({
 	selector: 'my-app',
 	template: `<router-outlet></router-outlet>`,
 })
 export class MainComponent {
-	constructor() {
-		
+	constructor(protected http: Http) {
+		http.get('https://api.github.com/repos/csutorasa/XOutput/releases').map(res => res.json()).toPromise().then(data => {
+			console.log(data);
+		}, err =>{
+			console.error(err);
+		});
 	}
 }
