406d2bf8b310fa2e4c9a5b6a5196fc3b5416662f
Ármin Csutorás <csutorasa@gmail.com>
2017-12-30 17:21:44 +0100

    Deploy updated

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,10 +2,10 @@ cd $(git rev-parse --show-toplevel) \
 && npm install \
 && mkdir -p "move" \
 && cp -f dist/* move \
-&& git checkout master \
+&& (git checkout master \
 && cp -f move/* dist \
 && git add . \
 && git commit -m "Release from develop" \
 && git push \
 && git checkout develop \
-&
+&)
