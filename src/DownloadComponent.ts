e95d073f7b9450599d2f560884007db29b2262d4
Ármin Csutorás <csutorasa@gmail.com>
2017-12-30 19:12:17 +0100

    DownloadCount log

--- src/DownloadComponent.ts
+++ src/DownloadComponent.ts
@@ -7,11 +7,29 @@ import { Observable } from 'rxjs/Rx';
 	template: `DownloadCount`,
 })
 export class DownloadComponent {
-	constructor(@Inject(HttpClient) protected httpClient: HttpClient) {
-		httpClient.get('https://api.github.com/repos/csutorasa/XOutput/releases').toPromise().then(data => {
-			console.log(data);
-		}, err =>{
-			console.error(err);
+
+	private readonly username: string = 'csutorasa';
+
+	constructor( @Inject(HttpClient) protected httpClient: HttpClient) {
+		httpClient.get('https://api.github.com/users/' + this.username + '/repos').toPromise().then((repos: any[]) => {
+			const promises: Promise<void>[] = [];
+			repos.forEach(repo => {
+				const fullName: string = repo.full_name;
+				promises.push(httpClient.get('https://api.github.com/repos/' + fullName + '/releases').toPromise().then((releases: any[]) => {
+					const donwloadCount = releases.map(release => <any>release.assets).map(assets => {
+						return assets.map(asset => asset.download_count)
+							.reduce((prev, curr, index, array) => {
+								return prev + curr;
+							});
+					}).reduce((prev, curr, index, array) => {
+						return prev + curr;
+					});
+					console.log(fullName, donwloadCount);
+				}));
+			});
+			return Promise.all(promises);
 		});
+
+
 	}
 }
